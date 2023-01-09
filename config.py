520e7c7c91fddf0f1d88a93b2bb843a806a43f36
kailasvg2003 <108928193+kailasvg2003@users.noreply.github.com>
2023-01-09 18:53:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("17059958"))
+API_HASH = getenv("61f1dff818fccd96e77db4477f360d0d")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("5948256402:AAFBM7GpPb7NpbSwghA4TzF4bOZVg3hA6Vg")
+OWNER_ID = int(getenv("5530347700"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = getenv("mongodb+srv://kailasvg:kailasvg@cluster04.gvju7bn.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN = getenv("Pk_links", None)
