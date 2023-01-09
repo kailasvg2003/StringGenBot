59dc1db9d7032f875afe7e43e3485f71a1f68e9f
kailasvg2003 <108928193+kailasvg2003@users.noreply.github.com>
2023-01-09 18:53:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ BOT_TOKEN = getenv("5948256402:AAFBM7GpPb7NpbSwghA4TzF4bOZVg3hA6Vg")
 OWNER_ID = int(getenv("5530347700"))
 
 MONGO_DB_URI = getenv("mongodb+srv://kailasvg:kailasvg@cluster04.gvju7bn.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("Pk_links", None)
+MUST_JOIN = getenv("Pk_links", True)
